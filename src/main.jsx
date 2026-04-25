70fd52a7cc818cb3f4706071d705d91b4351067a
YUNA9627 <yuna2175@naver.com>
2026-04-25 21:09:51 +0900

    style: Radix UI Themes 전역 적용

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Theme>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Theme>
   </StrictMode>,
-)
+);
