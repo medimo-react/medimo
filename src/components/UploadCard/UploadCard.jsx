28ebf504a3a8414ea0f8fe961ee9c67ef75a31a9
유지수 <jsyu333@gmail.com>
2026-04-27 21:22:09 +0900

    feat: 처방전 업로드 버튼 컴포넌트 디자인 작업

--- src/components/UploadCard/UploadCard.jsx
+++ src/components/UploadCard/UploadCard.jsx
@@ -1,12 +1,14 @@
 import styles from "./UploadCard.module.css";
 import Card from "../Card/Card.jsx";
 import UploadDropBox from "./UploadDropBox.jsx";
+import UploadActionBtn from "./UploadActionBtn.jsx";
 
 const UploadCard = () => {
   return (
       <Card radius={'sm'}>
         <p className={styles.title}>처방전 업로드</p>
         <UploadDropBox/>
+        <UploadActionBtn/>
       </Card>
   )
 }
