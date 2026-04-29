7c1aa4ec2f237be8374f7d45721b374d66047c7c
YUNA9627 <yuna2175@naver.com>
2026-04-29 22:46:06 +0900

    feat: Badge outline 옵션 추가

--- src/components/Badge/Badge.jsx
+++ src/components/Badge/Badge.jsx
@@ -1,24 +1,32 @@
 import styles from "./Badge.module.css";
 
 function Badge({
+  children,
   variant = "primary",
   size = "md",
-  children,
+  outline = false,
   className = "",
   ...props
 }) {
+  
   const Badge = [styles.badge, styles[variant], styles[size], className]
     .filter(Boolean)
     .join(" ");
 
   return (
     <span
-      className={`${styles.badge} ${styles[variant]} ${styles[size]} ${className}`}
+      className={`
+        ${styles.badge}
+        ${styles[variant]}
+        ${styles[size]}
+        ${outline ? styles.outline : ""}
+        ${className}
+      `}
       {...props}
     >
       {children}
     </span>
   );
-}
+};
 
 export default Badge;
