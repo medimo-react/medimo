54d902289d4741c24092fea61b3513b45236edb3
유지수 <jsyu333@gmail.com>
2026-05-01 13:36:35 +0900

    feat: PageHeader > description props 추가 및 스타일 추가

---
+++ src/pages/History/History.jsx
@@ -0,0 +1,12 @@
+import Container from "../../components/Container/Container.jsx";
+import PageHeader from "../../components/PageHeader/PageHeader.jsx";
+
+const History = () => {
+  return (
+      <Container>
+        <PageHeader title={"AI 분석 기록"} />
+      </Container>
+  )
+}
+
+export default History;
